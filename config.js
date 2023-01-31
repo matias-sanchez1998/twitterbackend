1bef23aa7ecbaccb7e6a7fed47210a914586d3ff
magomatiassss <magomatiassss@gmail.com>
2023-01-31 19:07:19 -0300

    hola

--- config.js
+++ config.js
@@ -3,4 +3,8 @@ import {config} from 'dotenv';
 config()
 
 export const PORT = process.env.PORT
-export const APYKEY = process.env.APYKEY
+export const BEARER = process.env.BEARER
+export const appKey = process.env.appKey
+export const appSecret = process.env.appSecret
+export const accessToken = process.env.accessToken
+export const accessSecret = process.env.accessSecret
--- controllers.js
+++ controllers.js
@@ -1,22 +1,45 @@
+import { TwitterApi } from "twitter-api-v2";
+// import { Client } from "twitter-api-sdk";
+import {
+  BEARER,
+  appKey,
+  appSecret,
+  accessToken,
+  accessSecret,
+} from "./config.js";
 
-import { Client } from "twitter-api-sdk";
-import {APYKEY} from './config.js'
+const client = new TwitterApi({
+  appKey: appKey,
+  appSecret: appSecret,
+  accessToken: accessToken,
+  accessSecret: accessSecret,
+});
 
-const client = new Client(APYKEY);
-
-
+// const client = new Client(BEARER);
 
 export const getMovements = async (req, res) => {
-    try {
-        const username = req.params.username;
-        const tweet = await client.users.findUserByUsername(username,{
-            'user.fields': ["created_at","description","location","name","username","url","profile_image_url","public_metrics"]
-        });
-        console.log(tweet);
-      res.json(tweet);
-    } catch (error) {
-      return res.status(500).json({
-        message: "algo fallo",
-      });
-    }
-  };
+  try {
+    const username = req.params.username;
+    // const tweet = await client.users.findUserByUsername(username,{
+    //     'user.fields': ["created_at","description","location","name","username","url","profile_image_url","public_metrics"]
+    // });
+    const tweet = await client.v2.userByUsername(username, {
+      "user.fields": [
+        "created_at",
+        "description",
+        "location",
+        "name",
+        "username",
+        "url",
+        "profile_image_url",
+        "public_metrics",
+      ],
+    });
+    console.log(tweet);
+    res.json(tweet);
+  } catch (error) {
+    return res.status(500).json({
+      message: "algo fallo",
+    });
+  }
+};
