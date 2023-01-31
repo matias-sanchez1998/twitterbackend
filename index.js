63a0aaea06c6a14e32bd8279ad9307e6f1e64dea
magomatiassss <magomatiassss@gmail.com>
2023-01-31 18:16:30 -0300

    hola

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ app.use("", movementsRoutes);
 
 //middlewares
 
+app.use("/", (req,res) => {
+  res.send("Bienvenido")
+})
 app.use((req, res, next) => {
   res.status(404).json({
     message: "endpoint not found",
